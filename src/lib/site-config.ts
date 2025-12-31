2bbb3d26ef52beb8ecd081c509dc0eb3fee8912e
Simon <omojide.o.j@gmail.com>
2025-12-31 08:31:13 +0100

    initial setup

---
+++ src/lib/site-config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+	name: 'Simon Abiodun Aina',
+	url: 'https://simonaina.com',
+	description:
+		'Automobile Operations Veteran and Full-Stack Developer building modern automotive retail and dealership software.',
+};
